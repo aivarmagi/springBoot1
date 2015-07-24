b28d6ce425a8f7d6f77fec619e69536ba431568f
Aivar Magi <aivar.magi@tieto.com>
2015-07-24 10:10:05 +0300

    SecurityConfiguration lisatud, eemaldatud default spring security osad (csrf protection, x-frame-options)

---
+++ src/main/java/boot/hello/SecurityConfiguration.java
@@ -0,0 +1,18 @@
+package boot.hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll()//allow all requests to the root url ("/")
+				.and().authorizeRequests().antMatchers("/console/**").permitAll(); //allow all requests to the H2 database console url ("/console/")
+
+		httpSecurity.csrf().disable(); //disable default Spring Security CSRF protection
+		httpSecurity.headers().frameOptions().disable(); //disable default X-Frame-Options in Spring Security
+	}
+}
